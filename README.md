6b4f36179d4141f731d005668d7935777f1e58a7
Dheeraj Yennam <dyenam@gmail.com>
2021-10-04 23:54:02 -0700

    initial commit of codebase

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# devoted-db
+# In Memory database implementation using a hashmap to store the key value pairs.
+
+## Installation.
+
+### Pre-Requisites
+
+Have a version of NODE(=>12) installed.
+
+### Install Required Packages.
+
+    # INSTALL Required NPM Packages
+    npm install
+
+    # To Start the program (will start a interactive shell)
+    npm start
+
+### Commands (not case sensitive)
+
+    # COMMAND: SET
+    # set a key value into the DB
+    # i.e
+    set color red
+
+    # COMMAND: GET
+    # Get the value given a key from the DB.
+    # i.e
+    get color
+
+    # COMMAND: COUNT [Time Complexity - O(n) - due to linear searches]
+    # Get the # of times a value appears in the DB.
+    # i.e
+    COUNT red
+
+    # COMMAND: DELETE
+    # Delete the key and its associated value.
+    # i.e
+    delete color
+
+    # COMMAND: BEGIN
+    # Create a new transaction.
+    # i.e
+    begin
+
+    # COMMAND: ROLLBACK
+    # ROLLBACKS all the transaction that were created after the 'BEGIN' command.
+    # i.e
+    rollback
+
+    # COMMAND: COMMIT
+    # COMMIT all the transaction that were created after the 'BEGIN' command to the DB.
+    # i.e
+    commit
+
+    # COMMAND: END
+    # Terminates the session.
+    # i.e
+    end
+
+## ISSUES:
+    Transactions/Rollback doesnt work as intended, it overwrites the original hashmap on every call.
---
+++ in-mem.js
@@ -0,0 +1,149 @@
+const HashMap = require("hashmap");
+let databaseMap = new HashMap();
+let transactionMap = new HashMap();
+
+class Database {
+  constructor() {
+    this.transactionsStoreStore = [];
+  }
+
+  // Set the key of the inMemDB entry
+  set(key, value) {
+    if (transactionMap.size) {
+      transactionMap.set(key, value);
+    } else {
+      // if it doesn't exist add it to the original databaseMap
+      databaseMap.set(key, value);
+    }
+  }
+
+  // Get the value of the key
+  get(key) {
+    if (transactionMap.size) {
+      console.log(transactionMap.get(key));
+    } else if (databaseMap.get(key)) {
+      console.log(databaseMap.get(key));
+    } else {
+      console.log("NULL");
+    }
+  }
+
+  // Counts the number of times the value occurs in the DB
+  count(value) {
+    const temp = databaseMap.values();
+    // Big O(n) = since its iterating over entire obj.
+    const filter = temp.filter((x) => x == value).length;
+    console.log(filter);
+  }
+
+  // Delete the key from the map
+  delete(key) {
+    if (databaseMap.has(key)) {
+      databaseMap.delete(key);
+    } else {
+      console.log("Nothing to delete");
+    }
+  }
+
+  // Begins a transaction
+  begin() {
+    // create a copy of existing hash, and add new transactions to it.
+    transactionMap.copy(databaseMap);
+  }
+
+  // Rollback the transaction
+  rollback() {
+    if (transactionMap.values()) {
+      transactionMap.clear();
+    } else {
+      console.log("Nothing to Rollback");
+    }
+  }
+
+  // Commit a transaction
+  commit() {
+    databaseMap.copy(transactionMap);
+    transactionMap.clear();
+  }
+
+  // Handle User Input for Various inMemDB Commands
+  handleInput(input) {
+    const inputRaw = input.split(" ");
+    const [cmd, ...args] = inputRaw;
+    switch (cmd.toUpperCase()) {
+      case "SET":
+        {
+          const [key, value] = args;
+          if (key && value) {
+            this.set(key, value);
+          } else {
+            console.log("SET command should include a key and a value.");
+          }
+        }
+        break;
+      // Get Value Given key
+      case "GET":
+        {
+          const [key] = args;
+          if (key) {
+            this.get(key);
+          } else {
+            console.log("GET command should include a key.");
+          }
+        }
+        break;
+      case "DELETE":
+        {
+          const [key] = args;
+          if (key) {
+            this.delete(key);
+          } else {
+            console.log("DELETE command should include a key.");
+          }
+        }
+        break;
+      case "COUNT":
+        {
+          const [value] = args;
+          if (value) {
+            this.count(value);
+          } else {
+            console.log("COUNT command requires a value to search for.");
+          }
+        }
+        break;
+      case "BEGIN":
+        this.begin();
+        break;
+      case "ROLLBACK":
+        this.rollback();
+        break;
+      case "COMMIT":
+        this.commit();
+        break;
+      case "END":
+        return true;
+      default:
+        console.log("Enter a valid input.");
+    }
+  }
+}
+
+const db = new Database();
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+var recursiveAsyncReadLine = function () {
+  readline.question(">> ", function (cmd) {
+    const split_cmd = cmd.split(" ");
+    if (split_cmd[0].toUpperCase() === "END") return readline.close();
+    db.handleInput(cmd);
+    recursiveAsyncReadLine(); //Calling this function again to ask new question
+  });
+};
+
+console.log("ENTER A VALID COMMAND");
+recursiveAsyncReadLine();
